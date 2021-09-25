f5b92a5a1a0b04917aff288de51e349852917dc3
hsynydn <ce.hsynydn@gmail.com>
2021-09-25 22:01:05 +0300

    feat: Create klogger kernel module

---
+++ src/klogger/klogger.c
@@ -0,0 +1,38 @@
+/**
+ * 	\file 		klogger.h
+ * 	\brief 		KeyLogger Kernel Module
+ * 	\details	A module which listens every keystrokes
+ * 				and saves into database
+ *	\author 	hsynydn
+ *	\date		25 September 2021
+ *	\warning 	For education purpose only
+ *	\copyright	GNU Public License
+ */
+
+#include "klogger.h"
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/**
+ *	\fn 	init_module
+ *	\brief 	module initialization
+ */
+int init_module(void){
+
+	MODULE_LICENSE(LICENCE);
+	MODULE_AUTHOR(AUTHOR);
+	MODULE_DESCRIPTION(DESCRIPTION);
+
+	printk(KERN_INFO "Hello World\n");
+	return 0;
+}
+
+/**
+ *	\fn 	cleanup_module
+ *	\brief 	module removing
+ */
+void cleanup_module(void){
+	printk(KERN_INFO "Goodbye World\n");
+}
---
+++ src/klogger/klogger.h
@@ -0,0 +1,17 @@
+/**
+ * 	\file 		klogger.h
+ * 	\brief 		Module macros and data types
+ *	\author 	hsynydn
+ *	\date		25 September 2021
+ *	\warning 	For education purpose only
+ *	\copyright	GNU General Public License
+ */
+
+#ifndef SRC_KLOGGER_KLOGGER_H_
+#define SRC_KLOGGER_KLOGGER_H_
+
+#define LICENCE 	"GNU/GPL"
+#define AUTHOR		"Hüseyin Aydın <huseyinaydin.ce@gmail.com>"
+#define DESCRIPTION "A kernel module which listens keystrokes"
+
+#endif /* SRC_KLOGGER_KLOGGER_H_ */
